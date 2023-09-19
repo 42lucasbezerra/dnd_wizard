3f06e79da8c2c4c398cd05bee00fc4e19b79ee3d
Lucas Bezerra <42lucas.bezerra@gmail.com>
2023-09-19 13:05:41 -0700

    added schools of magic

--- frontend/src/components/SpellModal.js
+++ frontend/src/components/SpellModal.js
@@ -52,8 +52,29 @@ const property_descriptions = {
         <b>Cantrips</b><br /><br />
         A cantrip is a spell that can be cast at will, without using a spell slot and without being prepared in advance. Repeated practice has fixed the spell in the caster’s mind and infused the caster with the magic needed to produce the effect over and over. A cantrip’s spell level is 0.
       </>
-    )
+    ),
+    abjuration: (<><b>Abjuration</b> spells are protective in nature, though some of them have aggressive uses. They create magical barriers, negate harmful effects, harm trespassers, or banish creatures to other planes of existence.</>),
+    conjuration: (<><b>Conjuration</b> spells involve the transportation of objects and creatures from one location to another. Some spells summon creatures or objects to the caster's side, whereas others allow the caster to teleport to another location. Some conjurations create objects or effects out of nothing.</>),
+    divination: (<><b>Divination</b> spells reveal information, whether in the form of secrets long forgotten, glimpses of the future, the locations of hidden things, the truth behind illusions, or visions of distant people or places.</>),
+    enchantment: (<><b>Enchantment</b> spells affect the minds of others, influencing or controlling their behavior. Such spells can make enemies see the caster as a friend, force creatures to take a course of action, or even control another creature like a puppet.</>),
+    evocation: (<><b>Evocation</b> spells manipulate magical energy to produce a desired effect. Some call up blasts of fire or lightning. Others channel positive energy to heal wounds.</>),
+    illusion: (<><b>Illusion</b> spells deceive the senses or minds of others. They cause people to see things that are not there, to miss things that are there, to hear phantom noises, or to remember things that never happened. Some illusions create phantom images that any creature can see, but the most insidious illusions plant an image directly in the mind of a creature.</>),
+    necromancy: (<><b>Necromancy</b> spells manipulate the energies of life and death. Such spells can grant an extra reserve of life force, drain the life energy from another creature, create the undead, or even bring the dead back to life. <br /><br />Creating the undead through the use of necromancy spells such as animate dead is not a good act, and only evil casters use such spells frequently.</>),
+    transmutation: (<><b>Transmutation</b> spells change the properties of a creature, object, or environment. They might turn an enemy into a harmless creature, bolster the strength of an ally, make an object move at the caster's command, or enhance a creature's innate healing abilities to rapidly recover from injury.</>),
 }
+
+const schools_of_magic = new Set([
+  "abjuration",
+  "conjuration",
+  "divination",
+  "enchantment",
+  "evocation",
+  "illusion",
+  "necromancy",
+  "transmutation  ",
+])
+
+
 
 export default class SpellInfoModal extends Component {
   constructor(props) {
@@ -113,7 +134,7 @@ export default class SpellInfoModal extends Component {
             <p><i>{typeParts.map((part, index) => {
               return (
                 <span key={index}>
-                  {part === '(ritual)' || part === 'cantrip' ? (
+                  {part === '(ritual)' || part === 'cantrip' || schools_of_magic.has(part) ? (
                     <span
                       className="component-link"
                       onClick={() => this.handlePropertyClick(part)}
